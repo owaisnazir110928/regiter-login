ac6ede7fcdc670cba8bd10547313be6740897d22
Owais Nazir Dar <63143570+owaisnazir110928@users.noreply.github.com>
2023-08-07 12:28:43 +0530

    ..

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,70 @@
-import React from "react";
-import { useState } from "react";
-function Login() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+import React, { useState } from "react";
 
-  const [password, setPassword] = useState("");
+function Login() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
   async function loginHandler(e) {
     e.preventDefault();
-    if (!email) {
-      setError("email is required");
+    
+    if (!email.trim()) {
+      setError("Email is required");
       return;
     }
+    
     if (!password || password.length < 6) {
-      setError("password is invalid should be 6 letter or long");
+      setError("Password is invalid. It should be at least 6 characters long.");
       return;
     }
-    if (password && email) {
-      setError("");
-      setFormData({
-        email: email,
-        password: password,
+
+    setError("");
+
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("your-api-url", {
+        method: "POST",
+        body: JSON.stringify(formData),
       });
-      console.log(formData);
-      try {
-        const response = await fetch("url", {
-          method: "POST",
-          body: JSON.stringify(formData),
-        });
+
+      if (response.ok) {
         const data = await response.json();
-        localStorage.setItem("authToken", data);
-      } catch (error) {
-        console.log(error);
+        console.log("Authentication token:", data.token);
+        localStorage.setItem("AuthToken",data.token)
+      } else if (response.status === 401) {
+        setError("Invalid email or password. Please check your credentials.");
+      } else {
+        setError("An error occurred.");
       }
+    } catch (error) {
+      console.log("Error:", error);
+      setError("An error occurred. Please try again later.");
     }
   }
+
   return (
     <div>
+      <h2>Login</h2>
       <form onSubmit={loginHandler}>
         {error && <div>{error}</div>}
-
         <div>
-          <span>Email:</span>
+          <label>Email:</label>
           <input
             type="email"
             value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
-          <span>Password:</span>
+          <label>Password:</label>
           <input
             type="password"
             value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div>
